0b989d95155f2a8a644d71151fab434e2f91401f
Jan Rohwer <jan.rohwer@gmail.com>
2021-09-27 21:23:58 +0200

    foldername fixed

--- .gocd/scripts/restart-stack.sh
+++ .gocd/scripts/restart-stack.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ssh -T -oStrictHostKeyChecking=no admin@ds.rohwer.loc \
-    "cd ${DOCKER_STACK_HOME}/stacks/homeautomation && sudo /usr/local/bin/docker-compose up -d"
+    "cd ${DOCKER_STACK_HOME}/stacks/home-automation && sudo /usr/local/bin/docker-compose up -d"
